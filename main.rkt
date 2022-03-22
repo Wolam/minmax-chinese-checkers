046c23f653249348edd7396650d864d62d075dd0
JosephV27 <jvalencianom08@estudiantec.cr>
2022-03-21 20:47:22 -0600

    Fixing bug in hash-positions2

--- main.rkt
+++ main.rkt
@@ -56,7 +56,7 @@
                   (let ((piece (hash-ref hash-positions2 jump-candidate)))
              (cond
                [(and (not piece) (not (member jump-candidate visited))) 
-               (cons (cons jump-candidate double-jumps) (valid-moves-by-hashes-offset white-pieces black-pieces jump-candidate #f moves (cons jump-candidate visited) color (+ double-jumps 5)))]
+               (cons (cons jump-candidate double-jumps) (valid-moves-by-hashes-offset white-pieces black-pieces jump-candidate #f moves (cons jump-candidate visited) color (+ double-jumps 7)))]
                [else moves])
    ))moves))
 
@@ -100,9 +100,6 @@
     moves)))
 
 (define piece-offsets '((-1 -1) (-1 0) (0 1) (0 -1) (1 0) (1 1)))
-
-;(-1 0) (0 1)
-; (0 -1) (1 0)
 
 (define (get-piece-offsets color)
   (if (eq? color 'black) '((-1 0) (0 1) (-1 -1))
@@ -221,14 +218,14 @@
             (send this remove target-piece)))
         (set! turn (if (eq? turn 'white) 'black 'white))
         (send piece set-location location)
-        (set! hash-positions2 (hash-set hash-positions2 (hash-ref hash-black-pieces (send piece get-id)) #f))
+        (set! hash-positions2 (hash-set hash-positions2 (car (hash-ref hash-black-pieces (send piece get-id))) #f))
         (set! hash-positions2 (hash-set hash-positions2 location #t))
         (set! hash-black-pieces (hash-set hash-black-pieces (send piece get-id) (cons location 0)))
         
         ; check if the player won
         (cond [(check-win? 'black board '("j6" "i7" "h8" "g9" "j7" "i8" "h9") '("j9" "j8" "i9")) (set-message "Player Wins")]
-              [(check-win? 'white board '("c0" "b1" "a2" "d0" "c1" "b2" "a3") '("a0" "a1" "b0")) (set-message "AI Wins")])
-        (set! initial-flag #f)
+              [(check-win? 'white board '("c0" "b1" "a2" "d0" "c1" "b2" "a3") '("a0" "a1" "b0")) (set-message "AI Wins")]
+              [else   (set! initial-flag #f)
         (when (eq? turn 'white)
           (when (< initial-move-counter 5)
             (let ((positions (second (get-initial-move))))
@@ -244,7 +241,8 @@
               (send (piece-at-location board (second positions)) set-location (car(third positions)))
               (position-piece board (piece-at-location board (car(third positions))))))
           (set! turn 'black)
-         )
+         )])
+      
         )
       
       ;; If the move is not valid it will be moved back.
@@ -276,17 +274,17 @@
     "b0" 2 "a1" 2 
     "c0" 3 "b1" 3 "a2" 3
     "d0" 4 "c1" 4 "b2" 4 "a3" 4
-    "e0" 5 "d1" 5 "c2" 5 "b3" 5 "a4" 5
-    "f0" 6 "e1" 6 "d2" 6 "c3" 6 "b4" 6 "a5" 6 
-    "g0" 7 "f1" 7 "e2" 7 "d3" 7 "c4" 7 "b5" 7 "a6" 7
-    "h0" 8 "g1" 8 "f2" 8 "e3" 8 "d4" 8 "c5" 8 "b6" 8 "a7" 8
-    "i0" 9 "h1" 9 "g2" 9 "f3" 9 "e4" 9 "d5" 9 "c6" 9 "b7" 9 "a8" 9
-    "j0" 10 "i1" 10 "h2" 10 "g3" 10 "f4" 10 "e5" 10 "d6" 10 "c7" 10 "b8" 10 "a9" 10
-    "j1" 11 "i2" 11 "h3" 11 "g4" 11 "f5" 11 "e6" 11 "d7" 11 "c8" 11 "b9" 11 
-    "j2" 12 "i3" 12 "h4" 12 "g5" 12 "f6" 12 "e7" 12 "d8" 12 "c9" 12
-    "j3" 13 "i4" 13 "h5" 13 "g6" 13 "f7" 13 "e8" 13 "d9" 13
-    "j4" 14 "i5" 14 "h6" 14 "g7" 14 "f8" 14 "e9" 14
-    "j5" 15 "i6" 15 "h7" 15 "g8" 15 "f9" 15 
+    "e0" 1 "d1" 5 "c2" 5 "b3" 5 "a4" 1
+    "f0" 1 "e1" 6 "d2" 6 "c3" 6 "b4" 6 "a5" 1 
+    "g0" 1 "f1" 1 "e2" 7 "d3" 7 "c4" 7 "b5" 1 "a6" 1
+    "h0" 1 "g1" 1 "f2" 8 "e3" 8 "d4" 8 "c5" 8 "b6" 1 "a7" 1
+    "i0" 1 "h1" 1 "g2" 1 "f3" 9 "e4" 9 "d5" 9 "c6" 1 "b7" 1 "a8" 1
+    "j0" 1 "i1" 1 "h2" 1 "g3" 10 "f4" 10 "e5" 10 "d6" 10 "c7" 1 "b8" 1 "a9" 1
+    "j1" 1 "i2" 1 "h3" 1 "g4" 11 "f5" 11 "e6" 11 "d7" 1 "c8" 1 "b9" 1 
+    "j2" 1 "i3" 1 "h4" 12 "g5" 12 "f6" 12 "e7" 12 "d8" 1 "c9" 1
+    "j3" 1 "i4" 1 "h5" 13 "g6" 13 "f7" 13 "e8" 1 "d9" 1
+    "j4" 1 "i5" 14 "h6" 14 "g7" 14 "f8" 14 "e9" 1
+    "j5" 1 "i6" 15 "h7" 15 "g8" 15 "f9" 1 
     "j6" 16 "i7" 16 "h8" 16 "g9" 16
     "j7" 17 "i8" 17 "h9" 17
     "j8" 18 "i9" 18
@@ -299,70 +297,69 @@
     "b0" 18 "a1" 18 
     "c0" 17 "b1" 17 "a2" 17
     "d0" 16 "c1" 16 "b2" 16 "a3" 16
-    "e0" 15 "d1" 15 "c2" 15 "b3" 15 "a4" 15
-    "f0" 14 "e1" 14 "d2" 14 "c3" 14 "b4" 14 "a5" 14 
-    "g0" 13 "f1" 13 "e2" 13 "d3" 13 "c4" 13 "b5" 13 "a6" 13
-    "h0" 12 "g1" 12 "f2" 12 "e3" 12 "d4" 12 "c5" 12 "b6" 12 "a7" 12
-    "i0" 11 "h1" 11 "g2" 11 "f3" 11 "e4" 11 "d5" 11 "c6" 11 "b7" 11 "a8" 11
-    "j0" 10 "i1" 10 "h2" 10 "g3" 10 "f4" 10 "e5" 10 "d6" 10 "c7" 10 "b8" 10 "a9" 10
-    "j1" 9 "i2" 9 "h3" 9 "g4" 9 "f5" 9 "e6" 9 "d7" 9 "c8" 9 "b9" 9 
-    "j2" 8 "i3" 8 "h4" 8 "g5" 8 "f6" 8 "e7" 8 "d8" 8 "c9" 8
-    "j3" 7 "i4" 7 "h5" 7 "g6" 7 "f7" 7 "e8" 7 "d9" 7
-    "j4" 6 "i5" 6 "h6" 6 "g7" 6 "f8" 6 "e9" 6
-    "j5" 5 "i6" 5 "h7" 5 "g8" 5 "f9" 5 
+    "e0" 1 "d1" 15 "c2" 15 "b3" 15 "a4" 15
+    "f0" 1 "e1" 14 "d2" 14 "c3" 14 "b4" 14 "a5" 14
+    "g0" 1 "f1" 1 "e2" 13 "d3" 13 "c4" 13 "b5" 13 "a6" 13
+    "h0" 1 "g1" 1 "f2" 12 "e3" 12 "d4" 12 "c5" 12 "b6" 12 "a7" 12
+    "i0" 1 "h1" 1 "g2" 1 "f3" 11 "e4" 11 "d5" 11 "c6" 11 "b7" 11 "a8" 1
+    "j0" 1 "i1" 1 "h2" 1 "g3" 10 "f4" 10 "e5" 10 "d6" 10 "c7" 10 "b8" 1 "a9" 1
+    "j1" 1 "i2" 1 "h3" 1 "g4" 9 "f5" 9 "e6" 9 "d7" 9 "c8" 9 "b9" 1
+    "j2" 1 "i3" 1 "h4" 8 "g5" 8 "f6" 8 "e7" 8 "d8" 8 "c9" 1
+    "j3" 1 "i4" 1 "h5" 7 "g6" 7 "f7" 7 "e8" 7 "d9" 1 
+    "j4" 1 "i5" 6 "h6" 6 "g7" 6 "f8" 6 "e9" 7
+    "j5" 1 "i6" 5 "h7" 5 "g8" 5 "f9" 5 
     "j6" 4 "i7" 4 "h8" 4 "g9" 4
     "j7" 3 "i8" 3 "h9" 3
     "j8" 2 "i9" 2
     "j9" 1 
     ))
-
 ; (substring "a1" 0 1) = a
 ; (substring "a1" 1 2) = 1
 (define white-distance-points
     (hash
-     "j" 10
-     "i" 15
-     "h" 20
+     "j" 5
+     "i" 10
+     "h" 15
      "g" 25
-     "f" 30
-     "e" 35
-     "d" 40
-     "c" 45
-     "b" 50
-     "a" 55
-      "0"  5
-      "1"  30 
-      "2"  30
-      "3"  30
+     "f" 25
+     "e" 30
+     "d" 35
+     "c" 40
+     "b" 45
+     "a" 50
+      "0"  1
+      "1"  25 
+      "2"  25
+      "3"  25
       "4"  25
-      "5"  20
-      "6"  15
-      "7"  10
-      "8"  10
-      "9"  5   ))
+      "5"  15
+      "6"  10
+      "7"  5
+      "8"  5
+      "9"  1   ))
 
 (define black-distance-points
     (hash
-     "a" 10
-     "b" 15
-     "c" 20
-     "d" 25
-     "e" 30
-     "f" 35
-     "g" 40
-     "h" 45
-     "i" 50
-     "j" 55
-     "0" 5
-     "1" 10
-      "2"  10
-      "3"  15
-      "4"  20
-      "5"  25
-      "6"  30
-      "7"  30
-      "8"  30
-      "9"  5   ))
+     "a" 5
+     "b" 10
+     "c" 15
+     "d" 20
+     "e" 25
+     "f" 30
+     "g" 35
+     "h" 40
+     "i" 45
+     "j" 50
+     "0" 1
+     "1" 5
+      "2"  5
+      "3"  10
+      "4"  15
+      "5"  20
+      "6"  25
+      "7"  25
+      "8"  25
+      "9"  1   ))
 
 (define initial-flag #t)
 
@@ -373,29 +370,23 @@
   (set! initial-move-counter (+ initial-move-counter 1))
     current-move)))
 
-
-    ; 0 '("a0" 0)
-
 (define (sum-black-points blacks)
   (let ((sum 0)) 
   (for ([(piece-id pos-and-jumps) blacks])
-   ;(displayln (hash-ref black-pieces-points (car pos-and-jumps))
-    ;(displayln (cdr pos-and-jumps))
      (set! sum (+ sum (hash-ref black-pieces-points (car pos-and-jumps))
                       ;(hash-ref black-distance-points (substring (car pos-and-jumps) 0 1))
                       ;(hash-ref black-distance-points (substring (car pos-and-jumps) 1 2))
+                      (cdr pos-and-jumps)
                       )))
-     ;(displayln sum))
     sum))
 
 (define (sum-white-points whites)
   (let ((sum 0)) 
   (for ([(piece-id pos-and-jumps) whites])
-    ;(displayln (cdr pos-and-jumps))
-    ;(displayln (hash-ref white-pieces-points (car pos-and-jumps)))
      (set! sum (+ sum (hash-ref white-pieces-points (car pos-and-jumps))
                       ;(hash-ref white-distance-points (substring (car pos-and-jumps) 0 1))
                       ;(hash-ref white-distance-points (substring (car pos-and-jumps) 1 2))
+                      (cdr pos-and-jumps)
                       )))
     sum))
 
